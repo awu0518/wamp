de88162a6624ba8c697d30b99765d00b3a16a0cc
Pari Patel <paripatel1515@gmail.com>
2026-05-06 00:09:12 -0400

    stop printing PythonAnywhere password during deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,6 @@ then
 fi
 
 echo "PA user = $PA_USER"
-echo "PA password = $DEMO_PA_PWD"
 
 echo "SSHing to PythonAnywhere."
 sshpass -p $DEMO_PA_PWD ssh -o "StrictHostKeyChecking no" $PA_USER@ssh.pythonanywhere.com << EOF
